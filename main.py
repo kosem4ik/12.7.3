6da61fac4709c2aba597a7a725510e69e4ae4f26
Константин Иделевич <kostyaidelevich@gmail.com>
2022-05-19 19:57:41 +0400

    fixed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 money = input('Введите сумму: ')
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = [value * float(money) for value in per_cent.values()]
-print('Максимальная сумма, которую вы можете заработать: ' + str(max(deposit)))
+print('Максимальная сумма, которую вы можете заработать - ' + str(max(deposit)))
